68c2e47b7289acacf8a948b1122670167bd4c867
Jam Adams <wodadehencou@gmail.com>
2019-05-28 17:36:11 +0800

    first version

---
+++ jam-zsh-plugin/alias.zsh
@@ -0,0 +1,103 @@
+
+cdls(){chdir $1;ls}
+
+alias work="cd $HOME/Workspace"
+alias q="exit"
+alias b="cd .."
+alias view="vim -R"
+alias vi="vim"
+alias cd='cdls'
+alias ssh='ssh -o serveraliveinterval=360 -X'
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+# alias ls='ls -F --color=auto'
+alias ll='ls -l'
+alias la='ls -a'
+alias grep='grep --color=auto'
+
+alias format_json='python -mjson.tool'
+
+#GOROOT=~/programs/go
+#GOROOT=~/Programs/go1.10
+#GOPATH=~/Programs/gopath
+#PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+
+#export GOROOT
+#export GOPATH
+#export PATH
+
+#function initgo () {
+#	export GOPATH=`pwd`
+#}
+
+
+function setgopath() {
+	DIR=`pwd`
+	echo $DIR
+	#echo $HOME
+
+	while [ $DIR != $HOME ]; do
+		#echo $DIR
+		if [ -d ${DIR}/src ]; then
+			break
+		else
+			DIR=`dirname ${DIR}`
+		fi
+	done
+	
+	if [ $DIR = $HOME ]; then
+		echo "Can't find a valid GOPATH"
+	else
+		export GOPATH=${DIR}
+		echo "Set env GOPATH as ${DIR}"
+		export PATH=${DIR}/bin:${PATH}
+		echo "Add PATH: ${DIR}/bin"
+	fi
+}
+
+function addgopath () {
+
+	DIR=`pwd`
+	echo $DIR
+	#echo $HOME
+	
+	while [ $DIR != $HOME ]; do
+		#echo $DIR
+		if [ -d ${DIR}/src ]; then
+			break
+		else
+			DIR=`dirname ${DIR}`
+		fi
+	done
+	
+	if [ $DIR = $HOME ]; then
+		echo "Can't find a valid GOPATH"
+	else
+		export GOPATH=${DIR}:${GOPATH}
+		echo "Set env GOPATH as ${DIR}"
+		export PATH=${DIR}/bin:${PATH}
+		echo "Add PATH: ${DIR}/bin"
+	fi
+}
+
+function gfw () {
+	export http_proxy=socks5://127.0.0.1:1080
+	export https_proxy=socks5://127.0.0.1:1080
+}
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+
+# setterm -term linux -regtabs 4
+
+# keyboard speed
+# xset r rate 300 40
+
+unsetopt share_history
+
+#PATH=~/.local/bin:$PATH
+#PATH=$ENV/script:$PATH
+#PATH=~/Programs/bin:$PATH
+#export PATH
+
---
+++ zshrc
@@ -0,0 +1,25 @@
+export ENV=$HOME/.env
+
+source $ENV/antigen/antigen.zsh
+
+antigen bundle robbyrussell/oh-my-zsh lib/
+
+antigen bundle git
+antigen bundle docker
+antigen bundle docker-compose
+antigen bundle golang
+antigen bundle brew
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle skywind3000/z.lua
+antigen bundle $ENV/jam-zsh-plugin --no-local-clone
+
+antigen theme tonyseek/oh-my-zsh-seeker-theme seeker
+
+antigen apply
+
+
+
+
